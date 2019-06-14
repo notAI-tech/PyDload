9c42b9332e51a8568092639222acd6222a6ec542
Praneeth <praneethbedapudi@gmail.com>
2019-06-15 03:33:25 +0530

    initial commit

---
+++ pydload/download.py
@@ -0,0 +1,26 @@
+import requests
+import progressbar
+import time
+
+mb = 1024 * 1024
+
+def dload(url, save_to_path, timeout=10, max_time=30):
+    request = requests.get(url, timeout=timeout, stream=True)
+    print(request.headers)
+    file_size = (float(request.headers['Content-length'])// mb) + 1
+
+    is_stopped = False
+
+    with open(save_to_path, 'wb') as f:
+        start_time = time.time()
+        for chunk in progressbar.progressbar(request.iter_content(mb), max_value=file_size, prefix='MB'):
+            f.write(chunk)
+            if time.time() - start_time >= max_time:
+                is_stopped = True
+                break
+    
+    if is_stopped:
+        print('Stopped due to excess time')
+    
+    else:
+        print('Succefully Downloaded to:', save_to_path)
