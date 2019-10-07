b23761c58ca158c758afa935d22a96dfacb7eb8a
Praneeth <praneethbedapudi@gmail.com>
2019-10-07 15:59:06 +0530

    re-organize __main__

--- pydload/__main__.py
+++ pydload/__main__.py
@@ -1,22 +1,3 @@
-from .download import dload
+from .download import cli
 
-import argparse
-
-parser = argparse.ArgumentParser(description='CLI for pydload')
-
-parser.add_argument('url', type=str, help='URL of the file to be downloaded.')
-
-parser.add_argument('save_to_path', type=str, nargs='?', help='save as file path/name')
-
-parser.add_argument('--max_time', type=int, help='Maximum time to be spent on download')
-parser.add_argument('--timeout', type=int, help='Reuest timeout')
-
-args = parser.parse_args()
-
-url = args.url
-save_to_path = args.save_to_path
-max_time = args.max_time
-timeout = args.timeout
-if not timeout: timeout=10
-
-dload(url, save_to_path=save_to_path, timeout=timeout, max_time=max_time, verbose=True)
+cli()
